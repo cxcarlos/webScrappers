51d13e1ac421fa8d1fc4d74fa6d3a9c971dadc27
Erick Garcia <egarcia@estratega.ec>
2021-05-06 09:33:40 -0500

    multiple request

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ var cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
-let data = [];
+//let data = [];
 
 const GetDateFormat = (date) => { 
     var month = (date.getMonth() + 1).toString();
@@ -47,40 +47,7 @@ const getIntoBdP = async (date, response) => {
                         await driver.wait(until.elementLocated(By.className("jmesa")),2000).then(async () => {
                             await driver.wait(until.elementLocated(By.id("table_1_row1")),2000).click().then(async () => {
                                 await driver.wait(until.elementLocated(By.id("solapa_movimientos")),5000).click().then(async () => {
-                                /*    let fechaDesde = await driver.wait(until.elementLocated(By.id("fechaDesde")));
-                                    let fechaHasta = await driver.wait(until.elementLocated(By.id("fechaHasta")));
-                                    driver.executeScript("arguments[0].removeAttribute('readonly') ",fechaDesde);
-                                    driver.executeScript("arguments[0].removeAttribute('readonly') ",fechaHasta);
-                                    driver.executeScript(`arguments[0].value='${minDate(date)}';`, fechaDesde);
-                                    driver.executeScript(`arguments[0].value='${maxDate(date)}';`, fechaHasta);
-                                    await driver.findElement(By.name("_eventId_consultar")).click().then(async () => { 
-                                       await driver.wait(until.elementLocated(By.id("table_1")),5000).findElements(By.css("tr")).then(async (rowList) =>{
-                                           const recolectData = async (rowList) => {                                             
-                                                for(var i=1 ; i < rowList.length; i++){
-                                                    await rowList[i].findElements(By.css("td")).then(async (colValue) => {
-                                                        await colValue[0].getText().then(async (coltxt) => {
-                                                            await colValue[4].getText().then( async (coltxt2) => {
-                                                                const newData = {
-                                                                    fecha: coltxt,
-                                                                    numero: coltxt2
-                                                                }
-                                                                data = [... data, newData]                                                                    
-                                                            })
-                                                        })
-                                                    })                                                          
-                                                }     
-                                            }
-                                            const jsonData = async(rowList) => {
-                                                await recolectData(rowList);
-                                                fs.writeFile('data.json', JSON.stringify(data),'utf8', (err) => { 
-                                                    if (err) throw err; 
-                                                    console.log('The file has been saved!'); 
-                                                });
-                                                response.status(202).json(data); 
-                                            }
-                                            jsonData(rowList);      
-                                        })
-                                    })     */                               
+                                
                                 })
                             })
                         })
@@ -94,42 +61,84 @@ const getIntoBdP = async (date, response) => {
 const getData = async (date, response) => {
     
     await driver.findElement(By.id("centrecontent")).then(async () => {
-        data = [];
-        console.log(date);
         let fechaDesde = await driver.wait(until.elementLocated(By.id("fechaDesde")));
         let fechaHasta = await driver.wait(until.elementLocated(By.id("fechaHasta")));
         driver.executeScript("arguments[0].removeAttribute('readonly') ",fechaDesde);
         driver.executeScript("arguments[0].removeAttribute('readonly') ",fechaHasta);
         driver.executeScript(`arguments[0].value='${minDate(date)}';`, fechaDesde);
         driver.executeScript(`arguments[0].value='${maxDate(date)}';`, fechaHasta);
-        await driver.findElement(By.name("_eventId_consultar")).click().then(async () => { 
-            await driver.wait(until.elementLocated(By.id("table_1")),5000).findElements(By.css("tr")).then(async (rowList) =>{
-                const recolectData = async (rowList) => {                                             
-                    for(var i=1 ; i < rowList.length; i++){
-                        await rowList[i].findElements(By.css("td")).then(async (colValue) => {
-                            await colValue[0].getText().then(async (coltxt) => {
-                                await colValue[4].getText().then( async (coltxt2) => {
-                                    const newData = {
-                                        fecha: coltxt,
-                                        numero: coltxt2
-                                    }
-                                    
-                                    data = [... data, newData]
+        
+        await driver.wait(until.elementLocated(By.name("_eventId_consultar"))).click().then(async () => { 
+            console.log("ENTRAR")
+     //       try {
+                await driver.wait(until.elementLocated(By.id("table_1"))).findElements(By.css("tr")).then(async (rowList) =>{
+                    console.log("table1");
+                    let data = [];
+                    const recolectData = async (rowList, data) => {                                             
+                        for(var i=1 ; i < rowList.length; i++){
+                            //console.log(rowList.getText());
+                            await rowList[i].findElements(By.css("td")).then(async (colValue) => {
+                                await colValue[0].getText().then(async (coltxt) => {
+                                    await colValue[4].getText().then( async (coltxt2) => {
+                                        const newData = {
+                                            fecha: coltxt,
+                                            numero: coltxt2
+                                        }
+                                        
+                                        data.push(newData);
+                                    })
                                 })
-                            })
-                        })                                                          
-                    }     
-                }
-                const jsonData = async(rowList) => {
-                    await recolectData(rowList);
-                    fs.writeFile('data.json', JSON.stringify(data),'utf8', (err) => { 
-                        if (err) throw err; 
-                        console.log('The file has been saved!'); 
-                    });
-                    response.status(202).json(data); 
-                }
-                jsonData(rowList);      
-            })
+                            })                                                          
+                        }  
+                    }
+
+                    const jsonData = async(rowList,data) => {
+                        await recolectData(rowList, data);
+                        fs.writeFile('data.json', JSON.stringify(data),'utf8', (err) => { 
+                            if (err) throw err; 
+                            console.log('The file has been saved!'); 
+                        });
+                        response.status(202).json(data); 
+                        
+                    }
+                    jsonData(rowList,data);      
+                })
+       /*     }
+            catch(error)
+            {
+                await driver.wait(until.elementLocated(By.id("table_1")),1000).findElements(By.css("tr")).then(async (rowList) =>{
+                    console.log("table1");
+                    let data = [];
+                    const recolectData = async (rowList, data) => {                                             
+                        for(var i=1 ; i < rowList.length; i++){
+                            await rowList[i].findElements(By.css("td")).then(async (colValue) => {
+                                await colValue[0].getText().then(async (coltxt) => {
+                                    await colValue[4].getText().then( async (coltxt2) => {
+                                        const newData = {
+                                            fecha: coltxt,
+                                            numero: coltxt2
+                                        }
+                                        
+                                        data.push(newData);
+                                    })
+                                })
+                            })                                                          
+                        } 
+                    }
+                    await recolectData(rowList, data);
+                    
+                    const jsonData = async(rowList,data) => {
+                        await recolectData(rowList, data);
+                        fs.writeFile('data.json', JSON.stringify(data),'utf8', (err) => { 
+                            if (err) throw err; 
+                            console.log('The file has been saved!'); 
+                        });
+                        response.status(202).json(data); 
+                    }
+                    jsonData(rowList,data);      
+                })
+            }
+          */  
         })
     })
 }
